a685c490ceca2103f1321359ab26a17d397d198b
eryxlee <eryxlee0901@gmail.com>
2015-09-17 14:10:53 +0800

    change thread to call webbrowser

--- python/http/test_wsgi_webob.py
+++ python/http/test_wsgi_webob.py
@@ -36,33 +36,16 @@ def app(environ, start_response):
 
 
 if __name__ == "__main__":
-    import time
-    import requests
-
-    from threading import Thread
-
-
-    class MyReq(Thread):
-        def __init__(self):
-            Thread.__init__(self)
-
-        def run(self):
-            time.sleep(10)
-            params = dict(
-                fullname='My Full Nmae',
-                address=['123', '456']
-            )
-            res = requests.post("http://127.0.0.1:6543?name=myname&mail=w<e>@163.com&mail=you@163.com", params)
-            print res.text
-
-
     httpd = make_server(
         '127.0.0.1',
         6543,
         app
     )
+    sa = httpd.socket.getsockname()
+    print "Serving HTTP on", sa[0], "port", sa[1], "..."
 
-    req = MyReq()
-    req.start()
+    import webbrowser
+    webbrowser.open('http://localhost:6543?name=myname&mail=w<e>@163.com&mail=you@163.com')
 
     httpd.handle_request()
+
